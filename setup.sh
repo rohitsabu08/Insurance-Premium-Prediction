b1b0b697fad007586298547ec24300b51397873e
rohitsabu08 <rohitsabu96@gmail.com>
2021-10-25 11:11:58 +0530

    setup changes

--- setup.sh
+++ setup.sh
@@ -5,9 +5,4 @@ headless = true\n\
 port = $PORT\n\
 enableCORS=false\n\
 \n\
-" > ~/.streamlit/config.toml[theme]
-primaryColor="#c54d4d"
-backgroundColor="#5a0216"
-secondaryBackgroundColor="#5c2d41"
-textColor="#f5f0f0"
-font="serif"
+" > ~/.streamlit/config.toml
